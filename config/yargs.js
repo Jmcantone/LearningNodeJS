5f9a7b5e5b6a992dafe2c639f106d5ce2ea8c213
Jmcantone <jmcantone@gmail.com>
2018-10-23 10:45:31 -0300

    Done

--- config/yargs.js
+++ config/yargs.js
@@ -1,33 +1,31 @@
-// const options = {
-//     description: {
-//         demand: true,
-//         alias: 'd',
-//         desc: 'Descripción de la tarea por hacer'
-//     }
-// }
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+};
+
+
 
 const argv = require('yargs')
     .command('crear', 'Crear un elemento por hacer', {
-        description: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        }
+        descripcion
     })
-    .command('actualizar', 'Actualiza el estado completo de una tarea', {
-        description: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        },
-        completado: {
-            default: true,
-            alias: 'c',
-            desc: 'Marca como completado o pendiente la tarea'
-        }
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra una tarea', {
+        descripcion
     })
     .help()
     .argv;
+
 
 module.exports = {
     argv
